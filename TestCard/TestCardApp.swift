80e830be975d293785ebbd4b48921c732d01cf53
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2021-12-15 08:56:24 -0300

    Initial Commit

---
+++ TestCard/TestCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCardApp.swift
+//  TestCard
+//
+//  Created by Guilherme Fernandes - pessoal on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TestCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
