3de37cc124bd2e07d5729642dcc7905b0d9813c6
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2021-12-16 11:32:25 -0300

    Simple presenting card for testing some SwiftUI design assets

--- TestCard/ContentView.swift
+++ TestCard/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.64, green: 0.61, blue: 1.00)
+                .ignoresSafeArea()
+            VStack {
+                Image("guilherme")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Guilherme Daniel")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+55 61 99992-9280", imageName: "phone.fill")
+                InfoView(text: "guilhermedfs11@gmail.com", imageName: "envelope.fill")
+        }
     }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone 13")
     }
 }
+
+
---
+++ TestCard/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  TestCard
+//
+//  Created by Guilherme Fernandes - pessoal on 15/12/21.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .overlay(
+                HStack {
+                    Image(systemName: imageName)
+                        .foregroundColor(.purple)
+                    Text(text)
+                })
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
